4680dcb0c8f3ddd65cd192c419aeda39ddd7497f
Victor1v <131626806+Victor1v@users.noreply.github.com>
2023-07-05 23:34:40 -0300

    Add files via upload

---
+++ PAGAR BARATO/app.js
@@ -0,0 +1,21 @@
+[
+    {
+        "src": "img/produto1.jpg"
+    },
+    {
+        "src": "img/produto2.jpg"
+    },
+    {
+        "src": "img/produto3.jpg"
+    },
+    {
+        "src": "img/produto4.jpg"
+    },
+    {
+        "src": "img/produto5.jpg"
+    },
+    {
+        "src": "img/produto6.jpg"
+    }
+]
+
